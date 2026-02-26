1b8c1ef565a99de62afa031beeb8ebb67694d96c
KC-Njoroge21 <kevinnjorogesic@gmail.com>
2026-02-26 09:30:34 +0300

    Initial Commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <section>
+      <form action="">
+        <div>
+          <input type="text" />
+        </div>
+        <div>
+          <input type="text" />
+        </div>
+      </form>
+    </section>
+  )
+}
+
+export default Login
